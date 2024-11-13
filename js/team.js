f26165c542375cf0e75212442190e57b309d981a
cathy-98 <119542561+cathy-98@users.noreply.github.com>
2024-11-13 00:18:13 -0300

    agregando js

---
+++ js/team.js
@@ -0,0 +1,23 @@
+console.log('Cargando el script del equipo-medico'); // mostrar en la consola
+
+//areglo de doctores []
+let doctores = [ // Objeto {}
+    {
+        image: "img/doctor1.webp", //  Atributo image
+        nombre: "Dr. Juan Pérez", //  Atributo nombre
+        especialidad: "Especialista en Cardiología", //  Atributo especialidad
+        descripcion: "El Dr. Juan Pérez cuenta con más de 20 años de experiencia en cardiología.",
+        genero:"m",
+    },
+    {
+        image: "img/doctora2.webp", //  Atributo image
+        nombre: "Dra. María López", //  Atributo nombre
+        especialidad: "Especialista en Consultas Médicas", //  Atributo especialidad
+        descripcion: "La Dra. María López es reconocida por su trabajo en consultas generales.",
+        genero:"f",
+    },
+    {
+        image: "img/doctor3.webp", //  Atributo image
+        nombre: "Dr. Carlos Martínez", //  Atributo nombre
+        especialidad: "Especialista en Hospitalización", //  Atributo especialidad
+        descripcion: "El Dr. Carlos Martínez se especializa en atención hospitalaria."
